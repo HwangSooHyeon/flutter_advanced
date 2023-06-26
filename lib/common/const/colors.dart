462dc87d163b49138b34c5100d9b9744980c9f4f
Soohyeon.Hwang <soohyeon@directional.net>
2023-06-26 14:25:42 +0900

    최초 커밋 및 주색상 정의

---
+++ lib/common/const/colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+// 주 색상
+const primaryColor = Color(0xFF22A45D);
+// 글자 색상
+const bodyTextColor = Color(0xFF868686);
+// 텍스트 필드 배경 색상
+const inputBackgroundColor = Color(0xFFFBFBFB);
+// 텍스트 필드 테두리 색상
+const inputBorderColor = Color(0xFFF3F2F2);
