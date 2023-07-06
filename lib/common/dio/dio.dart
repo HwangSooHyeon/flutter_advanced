7e501338f3e4269adb7f5f6a32133759e2fcdd93
Soohyeon.Hwang <soohyeon@directional.net>
2023-07-06 09:07:10 +0900

    feature: restaurant detail screen에 refrofit 적용

---
+++ lib/common/dio/dio.dart
@@ -0,0 +1,110 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_advanced/common/const/data.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class CustomInterceptor extends Interceptor {
+  final FlutterSecureStorage storage;
+
+  CustomInterceptor({
+    required this.storage,
+  });
+
+  // 1) 요청을 보낼 때
+  // 요청이 보내질 때마다
+  // 만약에 요청의 Header에 accessToken: true라는 값이 있다면
+  // 실제 토큰을 가져와서 (storage에서) authorization: bearer $token으로
+  // 헤더를 변경한다.
+  @override
+  void onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    print('[REQ] [${options.method}] ${options.uri}');
+
+    if (options.headers['accessToken'] == 'true') {
+      // 헤더 삭제
+      options.headers.remove('accessToken');
+
+      final token = await storage.read(key: accessTokenKey);
+
+      // 실제 토큰으로 대체
+      options.headers.addAll({
+        'authorization': 'Bearer $token',
+      });
+    }
+
+    if (options.headers['refreshToken'] == 'true') {
+      // 헤더 삭제
+      options.headers.remove('refreshToken');
+
+      final token = await storage.read(key: refreshTokenKey);
+
+      // 실제 토큰으로 대체
+      options.headers.addAll({
+        'authorization': 'Bearer $token',
+      });
+    }
+
+    return super.onRequest(options, handler);
+  }
+
+  // 2) 응답을 받을 때
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    print('[RES] [${response.requestOptions.method}] ${response.requestOptions.uri}');
+    return super.onResponse(response, handler);
+  }
+
+  // 3) 에러가 났을 때
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) async {
+    // 401 에러가 났을 때 (status code)
+    // 토큰을 재발급 받는 시도를 하고 토큰이 재발급되면
+    // 다시 새로운 토큰으로 요청을 한다.
+    print('[ERR] [${err.requestOptions.method}] ${err.requestOptions.uri}');
+
+    final refreshToken = await storage.read(key: refreshTokenKey);
+
+    // refreshToken 아예 없으면
+    // 당연히 에러를 던진다.
+    if (refreshToken == null) {
+      // 에러를 던질때는 handler.reject()를 사용한다.
+      return handler.reject(err);
+    }
+
+    final isStatus401 = err.response?.statusCode == 401;
+    final isPathRefresh = err.requestOptions.path == '/auth/token';
+
+    if (isStatus401 && !isPathRefresh) {
+      final dio = Dio();
+      try {
+        final response = await dio.post(
+          'http://$ip/auth/token',
+          options: Options(
+            headers: {
+              'authorization': 'Bearer $refreshToken',
+            },
+          ),
+        );
+        final accessToken = response.data['accessToken'];
+
+        final options = err.requestOptions;
+
+        //토큰 변경
+        options.headers.addAll({
+          'authorization': 'Bearer $accessToken',
+        });
+
+        await storage.write(key: accessTokenKey, value: accessToken);
+
+        // 요청 재전송
+        final retriedResponse = await dio.fetch(options);
+
+        return handler.resolve(retriedResponse);
+      } on DioException catch (e) {
+        return handler.reject(err);
+      }
+    }
+    return handler.reject(err);
+  }
+}
--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -1,10 +1,12 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced/common/const/data.dart';
+import 'package:flutter_advanced/common/dio/dio.dart';
 import 'package:flutter_advanced/common/layout/default_layout.dart';
 import 'package:flutter_advanced/product/component/product_card.dart';
 import 'package:flutter_advanced/restaurant/component/restaurant_card.dart';
 import 'package:flutter_advanced/restaurant/model/restaurant_detail_model.dart';
+import 'package:flutter_advanced/restaurant/repository/restaurant_repository.dart';
 
 class RestaurantDetailScreen extends StatelessWidget {
   final String id;
@@ -14,45 +16,41 @@ class RestaurantDetailScreen extends StatelessWidget {
     required this.id,
   }) : super(key: key);
 
-  Future<Map<String, dynamic>> getRestaurantDetail() async {
+  Future<RestaurantDetailModel> getRestaurantDetail() async {
     final dio = Dio();
-    final accessToken = await storage.read(key: accessTokenKey);
-    final response = await dio.get(
-      'http://$ip/restaurant/$id',
-      options: Options(
-        headers: {
-          'authorization': 'Bearer $accessToken',
-        },
+    dio.interceptors.add(
+      CustomInterceptor(
+        storage: storage,
       ),
     );
-    return response.data;
+    final repository = RestaurantRepository(
+      dio,
+      baseUrl: 'http://$ip/restaurant',
+    );
+    return repository.getRestaurantDetail(id: id);
   }
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '불타는 떡볶이',
-      child: FutureBuilder<Map<String, dynamic>>(
+      child: FutureBuilder<RestaurantDetailModel>(
         future: getRestaurantDetail(),
-        builder: (_, AsyncSnapshot<Map<String, dynamic>> snapshot) {
+        builder: (_, AsyncSnapshot<RestaurantDetailModel> snapshot) {
           if (!snapshot.hasData) {
             return Center(
               child: CircularProgressIndicator(),
             );
           }
 
-          final item = RestaurantDetailModel.fromJson(
-            snapshot.data!,
-          );
-
           return CustomScrollView(
             slivers: [
               renderTop(
-                model: item,
+                model: snapshot.data!,
               ),
               renderLabel(),
               renderProducts(
-                products: item.products,
+                products: snapshot.data!.products,
               ),
             ],
           );
